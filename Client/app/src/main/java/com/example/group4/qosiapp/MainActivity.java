e310bb8c73b72f5da3b0730e9c06f32f17ba108f
Jpvsilva <a75280@alunos.uminho.pt>
2018-05-11 11:37:18 +0100

    Add Client

---
+++ Client/app/src/main/java/com/example/group4/qosiapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.group4.qosiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    //Establishing TCP connection with server
+
+
+    //Receiving data and present them in screen
+
+    /**Create graphic with statics like:
+     * how many websites were distinct visited (distinct because some websites load ads from another locations)
+     * percentage of ads in total
+     * percentage per website ???
+     * bandwidth consumption by the ads ???
+     */
+}
