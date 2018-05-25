1b569f54da80f8c6477d7d30fdd74ff86144000f
Jpvsilva <a75280@alunos.uminho.pt>
2018-05-25 13:10:28 +0100

    Add Filter and new DB of ads

--- Server/AppServer/src/appserver/ServerWorker.java
+++ Server/AppServer/src/appserver/ServerWorker.java
@@ -7,9 +7,12 @@ package appserver;
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 /**
@@ -30,16 +33,17 @@ public class ServerWorker extends Thread{
     
     @Override
     public void run(){
+        int counter = 0;
         String lineS,line,trim;
         String fileName = "output.txt";
-        String fileName2 = "easylist.txt";
+        String fileName2 = "adlist.txt";
         
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
             PrintWriter out = new PrintWriter(cs.getOutputStream(),true);
             while ((lineS = in.readLine()) != null) {
                 System.out.println(lineS);
-                if(lineS.equals("Start")){
+                if(lineS.toLowerCase().equals("start")){
                     // Get all information from mitmproy
                     String command = "cmd /c start " + "mitmdump > output.txt";
                     ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "mitmdump > output.txt");
@@ -48,7 +52,7 @@ public class ServerWorker extends Thread{
                     out.println("Iniciado");
                     out.flush();
                 }
-                else if(lineS.equals("Stop")){
+                else if(lineS.toLowerCase().equals("stop")){
                     Process rt2 = Runtime.getRuntime().exec("TASKKILL /F /IM mitmdump.exe");
                     
                     //Read Traffic generated from user
@@ -58,7 +62,7 @@ public class ServerWorker extends Thread{
                         while ((line = bufferedReader.readLine()) != null) {
                             if(line.contains("GET")){
                                 trim = line.substring(line.lastIndexOf("GET") + 4);
-                                System.out.println(trim);
+                                //System.out.println(trim);
                                 urls.add(trim);
                             }
                         }
@@ -69,12 +73,34 @@ public class ServerWorker extends Thread{
                         //Read AdsDB from file
                         try (BufferedReader bufferedReader2 = new BufferedReader(fileReader2)) {
                             while ((line = bufferedReader2.readLine()) != null) {
-                                adsDB.add(line);
+                                if(!line.equals("\n")) adsDB.add(line);
                             }
                         }
+                        
+                        System.out.println("AdsDB list size: " + adsDB.size());
+
                         out.println("Foram lidos o tráfego e base de dados");
                         out.flush();
-                         System.exit(0);
+                        Iterator<String> iterator = urls.iterator();
+                        Iterator<String> iterator2 = adsDB.iterator();
+                        
+                        while(iterator.hasNext()) {
+                            String url = iterator.next();
+                            while(iterator2.hasNext()){
+                                String ad = iterator2.next();
+                                if(url.contains(ad) && (ad.length() > 1)){
+                                    System.out.println("URL: " + url);
+                                    counter++;
+                                    System.out.println("    returned the ad: " + ad);
+                                    break;
+                                }
+                            }
+                            iterator2 = adsDB.iterator();
+                        }
+                        
+                        System.out.println("Total amount of " + counter + " ads");
+                        
+                        System.exit(0);
                     }else{
                         out.println("Sem Trafego");
                         out.flush();
